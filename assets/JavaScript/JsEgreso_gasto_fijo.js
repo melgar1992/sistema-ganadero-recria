53640fbd97378685b302ff391aeef32514834098
erika fernanda <ericka_nanda@hotmail.com>
2019-10-23 21:14:26 -0400

    Formulario Egreso Gasto Fijos
    Se realizo el guardar del formulario egreso gasto fijo

---
+++ assets/JavaScript/JsEgreso_gasto_fijo.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var base_url = $("#base_url").val();
+
+    $(document).on("click", ".btn-check", function() {
+
+        empleado = $(this).val();
+        infoempleado = empleado.split("*");
+        $("#id_empleado").val(infoempleado[0]);
+        $("#empleado").val(infoempleado[1]);
+        $("#modal-default").modal("hide");
+    });
+})
