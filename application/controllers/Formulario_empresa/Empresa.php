7743b700d72bcd2c955f2ebd47a466a02d2dbdd0
miguel melgar <46229988+melgar1992@users.noreply.github.com>
2022-08-10 10:17:37 -0400

    Crud datos empresa

--- application/controllers/Formulario_empresa/Empresa.php
+++ application/controllers/Formulario_empresa/Empresa.php
@@ -10,125 +10,27 @@ class Empresa extends BaseController
 
         $this->loadView('Empresa', '/form/formulario_empresa/list', $data);
     }
-    public function guardarGanadero()
+    public function guardarEmpresa()
     {
-        $nombres = $this->input->post('nombre');
-        $apellidos = $this->input->post('apellidos');
-        $ci = $this->input->post('ci');
+        $nombre = $this->input->post('nombre');
         $telefono = $this->input->post('telefono');
-        $tipo_ganadero = $this->input->post('tipo_ganadero');
-      
+        $descripcion = $this->input->post('descripcion');
+        $id = $this->input->post('id');
 
         $this->form_validation->set_rules("nombre", "Nombre", "required");
-        $this->form_validation->set_rules("apellidos", "Apellidos", "required");
-        $this->form_validation->set_rules(
-            'ci',
-            'ci',
-            array(
-                'required',
-                array('validarCi', array($this->Ganadero_model, 'validarCi'))
-            ),
-            array('validarCi' => 'Carnet de identidad ya esta siendo ocupado')
-        );
         $this->form_validation->set_rules("telefono", "Telefono", "required");
 
 
         if ($this->form_validation->run()) {
-
-
-            $datospersona = array(
-                'nombres' => $nombres,
-                'apellidos' => $apellidos,
-                'carnet_identidad' => $ci,
-                'telefono' => $telefono,
-
-            );
-            $datosganadero = array(
-                'tipo_ganadero' => $tipo_ganadero,
-                'estado' => "1"
-            );
-           
-
-            if ($this->Ganadero_model->guardarGanadero($datospersona, $datosganadero)) {
-                redirect(base_url() . "Formularios_Generales/ganadero");
+            if ($id == 0) {
+                $this->Empresa_model->guardarEmpresa($nombre, $telefono, $descripcion);
+                $this->index();
             } else {
-                $this->session->set_flashdata("error", "No se pudo guardar la informacion");
+                $this->Empresa_model->actualizar($id, $nombre, $telefono, $descripcion);
+                $this->index();
             }
         } else {
             $this->index();
         }
-    }
-
-    public function editar($id_ganadero)
-    {
-        $data = array(
-            'ganadero' => $this->Ganadero_model->getGanadero($id_ganadero),
-        );
-        $this->loadView('Ganadero', '/form/formulario_generales/ganadero/editar', $data);
-    }
-
-    public function actualizarGanadero()
-    {
-        $id_ganadero = $this->input->post("id_ganadero");
-        $id_persona = $this->input->post("id_persona");
-        $nombres = $this->input->post('nombre');
-        $apellidos = $this->input->post('apellidos');
-        $ci = $this->input->post('ci');
-        $telefono = $this->input->post('telefono');
-
-
-        $ganaderoActual = $this->Ganadero_model->getGanadero($id_ganadero);
-        if ($ci == $ganaderoActual->carnet_identidad) {
-
-            $this->form_validation->set_rules("nombre", "Nombre", "required");
-            $this->form_validation->set_rules("apellidos", "Apellidos", "required");
-            $this->form_validation->set_rules("ci", "CI", "required");
-            $this->form_validation->set_rules("telefono", "Telefono", "required");
-        } else {
-            $this->form_validation->set_rules("nombre", "Nombre", "required");
-            $this->form_validation->set_rules("apellidos", "Apellidos", "required");
-            $this->form_validation->set_rules(
-                'ci',
-                'ci',
-                array(
-                    'required',
-                    array('validarCi', array($this->Ganadero_model, 'validarCi'))
-                ),
-                array('validarCi' => 'Carnet de identidad ya esta siendo ocupado')
-            );
-            $this->form_validation->set_rules("telefono", "Telefono", "required");
-        }
-
-        if ($this->form_validation->run()) {
-
-            $datospersona = array(
-
-                'nombres' => $nombres,
-                'apellidos' => $apellidos,
-                'carnet_identidad' => $ci,
-                'telefono' => $telefono,
-
-            );
-            $datosganadero = array(
-                'estado' => "1"
-            );
-
-            if ($this->Transportista_model->actualizar($id_ganadero, $id_persona, $datospersona, $datosganadero)) {
-                redirect(base_url() . "Formularios_Generales/Ganadero");
-            } else {
-                $this->session->set_flashdata("error", "No se pudo actualizar la informacion");
-                redirect(base_url() . "Formularios_Generales/Ganadero/editar" . $id_ganadero);
-            }
-        } else {
-            $this->Editar($id_ganadero);
-        }
-    }
-    public function borrar($id_ganadero)
-    {
-        $data = array(
-            'estado' => "0",
-        );
-        $this->Ganadero_model->borrar($id_ganadero, $data);
-        echo "Formularios_Generales/Ganadero";
     }
 }
--- application/views/form/formulario_empresa/list.php
+++ application/views/form/formulario_empresa/list.php
@@ -14,8 +14,60 @@
                 </div>
             </div>
             <div class="box-body">
+                <p>Los siguientes datos son de la empresa</p>
+                <br>
 
-                <?php print_r($empresa) ?>
+
+                <form method="POST" action="<?php echo base_url(); ?>Formulario_empresa/Empresa/guardarEmpresa" id="empleado" class="form-horizontal form-label-left">
+                    <input type="hidden" name="id" id="id" value="<?php if (isset($empresa)) {
+                                                                        echo $empresa['id_datos_empresa'];
+                                                                    } else {
+                                                                        echo '0';
+                                                                    } ?>">
+                    <div class="form-group <?php echo !empty(form_error("nombre")) ? 'has-error' : ''; ?>">
+                        <label for="nombre" class="control-label col-md-3 col-sm-3 col-xs-12">Nombres: <span class="required">*</span></label>
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <input type="text" name="nombre" value="<?php if (isset($empresa)) {
+                                                                        echo $empresa['nombre'];
+                                                                    } ?>" id=nombre required="required" class="form-control col-md-3 col-xs-12" placeholder="">
+                            <?php echo form_error("nombre", "<span class='help-block col-md-4 cols-xs-12 '>", "</span>"); ?>
+                        </div>
+                    </div>
+                    <div class="form-group <?php echo !empty(form_error("telefono")) ? 'has-error' : ''; ?>">
+                        <label for="telefono" class="control-label col-md-3 col-sm-3 col-xs-12">Numero de Telefono: <span class="required">*</span></label>
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <input type="number" name="telefono" value="<?php if (isset($empresa)) {
+                                                                            echo $empresa['telefono'];
+                                                                        } ?>" id=telefono required="required" class="form-control col-md-3 col-xs-12" placeholder="">
+                            <?php echo form_error("telefono", "<span class='help-block col-md-4 cols-xs-12 '>", "</span>"); ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="descripcion" class="control-label col-md-3 col-sm-3 col-xs-12">descripcion: </label>
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <textarea rows="3" type="text" name="descripcion" value="" id=descripcion class="form-control col-md-3 col-xs-12" placeholder=""><?php if (isset($empresa)) {
+                                                                                                                                                                    echo $empresa['descripcion'];
+                                                                                                                                                                } ?></textarea>
+                            <?php echo form_error("descripcion", "<span class='help-block col-md-4 cols-xs-12 '>", "</span>"); ?>
+                        </div>
+                    </div>
+
+                    <div class="in_solid"></div>
+
+                    <br>
+
+                    <div class="form-group">
+
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                            <button class="btn btn-primary btn-flat" type="reset">Borrar</button>
+                            <button type="submit" id="guardar" class="btn btn-success">Guardar</button>
+
+                        </div>
+                    </div>
+
+                    <div class="in_solid"></div>
+
+                </form>
             </div>
         </div>
     </section>
